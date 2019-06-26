ec4320f47fbef886f6ad57dca664593cb88b1736
abodu <abodu@qq.com>
2019-06-26 17:29:27 +0800

    fix:update tools/bld_commonlib.sh

--- tools/bld_commonlib.sh
+++ tools/bld_commonlib.sh
@@ -8,12 +8,39 @@
 # VERNO: 0.0.1
 # LUPTS: 2019-06-26 17:10:57
 #=================================================================
-
+#log utils
+logInfo() {
+  logger -s "[]"
+}
+logErr() {
+  LL=ERR logInfo "$@"
+}
 #OS Detection
+OS_TYPE=$(uname)
+OS_ID=
+OS_VERSION_ID=
+PKG=
+CMAKE=
+
 if [ -e /etc/os-release ]; then
-if [ "X$(uname)" == "XLinux" ]; then
-  OS_ID=$(cat /etc/os-release | awk -F'=' '/^ID=/{print $2}' | sed 's/"//g')
-  OS_VERSION_ID=$(cat /etc/os-release | awk -F'=' '/^VERSION_ID=/{print $2}' |sed 's/"//g')
-fi
+  if [ "X${OS_TYPE}" == "XLinux" ]; then
+    OS_ID=$(cat /etc/os-release | awk -F'=' '/^ID=/{print $2}' | sed 's/"//g')
+    OS_VERSION_ID=$(cat /etc/os-release | awk -F'=' '/^VERSION_ID=/{print $2}' | sed 's/"//g')
+  fi
 fi
 
+case $OS_ID in
+ubuntu | debian | deepin)
+  :
+  PKG=deb
+  CMAKE=cmake
+  ;;
+centos | rhel | fedora | opensuse*)
+  :
+  PKG=rpm
+  CMAKE=cmake3
+  ;;
+*)
+  logErr "not support [$OS_ID]"
+  ;;
+esac
